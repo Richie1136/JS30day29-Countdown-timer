4cd54f0fd411997cc5b1b7161c4be23b759b446b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-19 13:49:33 -0400

    Displaying the end time and converting the hours to normal time instead of miltary time

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 let countdown;
 
 const timerDisplay = document.querySelector('.display-time-left')
+const endTime = document.querySelector('.display-end-time')
+
 
 const displayTimeLeft = (seconds) => {
   let minutes = Math.floor(seconds / 60)
   let hours = Math.floor(seconds / 3600)
   let remainderSeconds = seconds % 60
   const display = `${hours}:${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+  document.title = display
   timerDisplay.textContent = display
+}
+const displayEndTime = (timestamp) => {
+  const end = new Date(timestamp)
+  const hour = end.getHours()
+  const minutes = end.getMinutes()
+  endTime.textContent = `Be Back at ${hour > 12 ? hour - 12 : ''} ${minutes}`
+  console.log(end)
+  console.log(hour)
 }
 const timer = (seconds) => {
   const now = Date.now()
   const then = now + seconds * 1000
   displayTimeLeft(seconds)
+  displayEndTime(then)
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000)
@@ -25,5 +37,5 @@ const timer = (seconds) => {
 
 }
 
-timer(124)
+timer(3000)
 
